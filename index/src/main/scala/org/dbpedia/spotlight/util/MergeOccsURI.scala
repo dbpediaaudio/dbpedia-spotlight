85c89653d6eb0fe6feb9f7ce4507260068de25bd
Your Name <sandroacoelho@gmail.com>
2014-01-28 20:35:13 -0200

    Mapping OCCS x URI

--- index/src/main/scala/org/dbpedia/spotlight/util/MergeOccsURI.scala
+++ index/src/main/scala/org/dbpedia/spotlight/util/MergeOccsURI.scala
@@ -1,7 +1,7 @@
 package org.dbpedia.spotlight.util
 
 import scala.io.Source
-import java.io.File
+import java.io.{FileWriter, File}
 
 class OCCS {
 
@@ -36,41 +36,47 @@ object MergeOccsURI {
     var map = Source.fromFile(args(0)).getLines().map(line => {
       (line.split("> <")(2).replace("> .", ""), line.split("> <")(0).replace("<", ""))
     }).toMap
-    var i = 1
-    var occs = Source.fromFile(args(1)).getLines().map(line => {
-      (line.split("\t")(1).trim(),
-        new OCCS(line.split("\t")(0), line.split("\t")(1), line.split("\t")(2), line.split("\t")(3), (line.split("\t")(line.split("\t").size - 1)).toInt))
-    }
-    ).toMap
-
-
-    map.foreach(m => {
-      val o = occs.get(m._1.trim())
-      if (!o.isEmpty)
-        o.get.uri = m._2
-    })
 
     val buffer = new StringBuilder
-    occs.foreach(o => {
 
-      buffer.append(o._2.surfaceForm + "\t" +
-        o._2.uri + "\t" +
-        o._2.label + "\t" +
-        o._2.context + "\t" +
-        o._2.count + "\n")
+    var processed = 0
 
-    })
+    Source.fromFile(args(1)).getLines().foreach(line => {
 
-    writeToFile(args(2), buffer.toString())
+      buffer.append(line.split("\t")(0) + "\t" +
+        getURI(map, line.split("\t")(1)) + "\t" +
+        line.split("\t")(2) + "\t" +
+        line.split("\t")(3) + "\t" +
+        (line.split("\t")(line.split("\t").size - 1)).toInt + "\n")
+
+      processed += 1
+
+      if (processed % 1000 == 0) {
+        writeToFile(args(2), buffer.toString())
+        buffer.clear()
+        println(" %d rows processed".format(processed))
+      }
+
+
+    }
+    )
+
+
     println("done!")
 
 
   }
 
 
+  def getURI(map: Map[String, String], uri: String): String = {
+    map.getOrElse(uri, uri)
+  }
+
   def writeToFile(p: String, s: String) {
-    val pw = new java.io.PrintWriter(new File(p))
-    try pw.write(s) finally pw.close()
+    val fw = new FileWriter(p, true);
+    fw.write(s);
+    fw.close()
+
   }
 
 }
